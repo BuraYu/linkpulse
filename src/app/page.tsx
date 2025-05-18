bf9d57ce190b8f50e125f85d3358b2fc686e1c65
BuraYu <burak.yueksel@hotmail.com>
2025-05-18 14:56:47 +0200

    add: supabase utility client, server and middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-dvh">
-      <h1>Hello World</h1>
+      <h1 className="text-2xl">Hello World</h1>
     </div>
   );
 }
